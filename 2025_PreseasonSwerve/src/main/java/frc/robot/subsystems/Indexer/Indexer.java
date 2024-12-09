726de8ce042227e1a22e6004ed37c6c4df40f11f
PaullyPocket <michaelpaulb09@gmail.com>
2024-12-09 15:46:23 -0800

    Indexer is all complete
    Now I gotta work on Beam brake :(((((((

--- 2025_PreseasonSwerve/src/main/java/frc/robot/subsystems/Indexer/Indexer.java
+++ 2025_PreseasonSwerve/src/main/java/frc/robot/subsystems/Indexer/Indexer.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.Indexer;
 
-
-import java.util.logging.Logger;
+import org.littletonrobotics.junction.Logger;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 import edu.wpi.first.wpilibj2.command.Command;
